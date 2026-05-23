68db3faea25c7fe1197792d32a7ea574ebdab86a
rbalsxzcv <rbalsxzcv2004@naver.com>
2026-05-23 18:01:01 +0900

    65%

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,18 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    int alpha[26] = {0};
 
+    int c;
+    while((c = fgetc(fp)) != EOF){
+        if(c >= 97 && c<= 122)
+            alpha[c-97]++;
+    }
+        
 
+    for(int i = 0;i<26;i++)
+        printf("%c: %d\n",(char) i+97,alpha[i]);
     fclose(fp);
+    return 0;
 }
 
